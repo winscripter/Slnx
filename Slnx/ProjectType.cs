c259ac25f9595c7119a9bde0636e29f26ffaa7fe
winscripter <142818255+winscripter@users.noreply.github.com>
2024-08-05 15:51:42 +0400

    2.0.1 (Debug -> Release)

--- Slnx/ProjectType.cs
+++ Slnx/ProjectType.cs
@@ -8,70 +8,70 @@
         /// <summary>
         /// C# Project (*.csproj)
         /// </summary>
-        CSharp,
+        CSharp = 0,
 
         /// <summary>
         /// Visual C++ Project (*.vcxproj)
         /// </summary>
-        VC,
+        VC = 1,
 
         /// <summary>
         /// VB.NET Project (*.vbproj)
         /// </summary>
-        VisualBasic,
+        VisualBasic = 2,
 
         /// <summary>
         /// F# Project (*.fsproj)
         /// </summary>
-        FSharp,
+        FSharp = 3,
 
         /// <summary>
         /// Microsoft Installer Project (*.vdproj)
         /// </summary>
-        Installer,
+        Installer = 4,
 
         /// <summary>
         /// Shared Project (*.shproj)
         /// </summary>
-        Shared,
+        Shared = 5,
 
         /// <summary>
         /// Database Project (*.dbproj)
         /// </summary>
-        Database,
+        Database = 6,
 
         /// <summary>
         /// Docker Compose Project (*.dcproj)
         /// </summary>
-        DockerCompose,
+        DockerCompose = 7,
 
         /// <summary>
         /// SQL Project (*.sqlproj)
         /// </summary>
-        Sql,
+        Sql = 8,
 
         /// <summary>
         /// JavaScript Application/ECMAScript Project (*.esproj)
         /// </summary>
-        JavaScript,
+        JavaScript = 9,
 
         /// <summary>
         /// .NET Core 2015 Project (*.xproj)
         /// </summary>
-        NetCore2015,
+        NetCore2015 = 10,
 
         /// <summary>
         /// Common MSBuild Project (*.msbuildproj)
         /// </summary>
-        Common,
+        Common = 11,
 
         /// <summary>
         /// Project (*.proj)
         /// </summary>
-        Project,
+        Project = 12,
 
         /// <summary>
-        /// Any project type which is not defined here.
+        /// Any project type which is not defined.
         /// </summary>
         Unknown = 0xFF
     }
